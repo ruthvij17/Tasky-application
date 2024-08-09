571962ecf7cf7aeedb5a91ae3e41f368cc618b82
ruthvij17 <ruthvijrchandan2004@gmail.com>
2024-08-09 08:33:27 +0530

    Project is in day3

---
+++ script.js
@@ -0,0 +1,41 @@
+// var state{
+//     taskList:[
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:"",
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:"",
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:"",
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:"",
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:"",
+//         }
+//     ]
+// };
+
+
+const state={
+    taskList:[],
+}
+
+const taskContents=document.getElementById()
