9fa4b6cacce54a603d51a5680a032526d536889f
ruthvij17 <ruthvijrchandan2004@gmail.com>
2024-08-11 13:12:29 +0530

    Done with the day5

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// var state{
-//     taskList:[
-//         {
-//             imageUrl:"",
-//             taskTitle:"",
-//             taskType:"",
-//             taskDescription:"",
-//         },
-//         {
-//             imageUrl:"",
-//             taskTitle:"",
-//             taskType:"",
-//             taskDescription:"",
-//         },
-//         {
-//             imageUrl:"",
-//             taskTitle:"",
-//             taskType:"",
-//             taskDescription:"",
-//         },
-//         {
-//             imageUrl:"",
-//             taskTitle:"",
-//             taskType:"",
-//             taskDescription:"",
-//         },
-//         {
-//             imageUrl:"",
-//             taskTitle:"",
-//             taskType:"",
-//             taskDescription:"",
-//         }
-//     ]
-// };
-
 
 const state = {
     taskList: [],
@@ -46,21 +11,22 @@ const taskModal = document.querySelector(".task__modal__body");
 
 //Template for the card on screen
 //element identifier key=
-const htmlTaskContent = ({ id, title, description, type, url }) => `
+const htmlTaskContent = ({id,title,description,type,url}) => `
     <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
         <div class="card shadow-sm task__card">
             <div class="card-header d-flex justify-content-end task__card__header">
-                <button type="button" class="btn btn-outline-info mr-1.5" name=${id}>
+                <button type="button" class="btn btn-outline-info mr-3" name=${id}>
                     <i class="fa fa-pencil-alt" name=${id}></i>
                 </button>
-                <button type="button" class="btn btn-outline-danger mr-1.5" name=${id}>
+                <button type="button" class="btn btn-outline-danger mr-1.5" name=${id} onclick="deleteTask.apply(this, arguments)">
                     <i class="fa fa-trash-alt" name=${id}></i>
                 </button>
             </div>
 
             <div class="card-body">
-                ${url &&
-    '<img width="100%" src=${url} alt="Card Image" class="card-img-top md-3 rounded-lg" />'
+                ${url 
+                    ?`<img width="100%" src=${url} alt="Card Image" class="card-img-top md-3 rounded-lg" />`
+                    :`<img width="100%" src="https://imgs.search.brave.com/vP12Jix8OyajdEksLOhYvhf9iArmiCUy89mNxo2CGO4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXZl/bG9wZXJzLmVsZW1l/bnRvci5jb20vZG9j/cy9hc3NldHMvaW1n/L2VsZW1lbnRvci1w/bGFjZWhvbGRlci1p/bWFnZS5wbmc" />`
     }
                 <h4 class="card-title task__card__title">${title
     }</h4>
@@ -72,7 +38,7 @@ const htmlTaskContent = ({ id, title, description, type, url }) => `
             </div>
 
             <div class="card-footer">
-                <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask">Open task</button>
+                <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask" onclick="openTask.apply(this, arguments)" id="${id}">Open task</button>
             </div>
         </div>
     </div>
@@ -81,12 +47,13 @@ const htmlTaskContent = ({ id, title, description, type, url }) => `
 
 // Modal body on clicking open task
 
-const htmlModalContent = ({ id, title, description, url }) => {
+const htmlModalContent = ({id,title,description,url}) => {
     const date = new Date(parseInt(id));
     return `
     <div id=${id}>
-    ${url &&
-        '<img width="100%" src=${url} alt="Card Image" class="img-fluid mb-3" />'
+    ${url 
+        ?`<img width="100%" src=${url} alt="Card Image" class="card-img-top md-3 rounded-lg" />`
+        :`<img width="100%" src="https://imgs.search.brave.com/vP12Jix8OyajdEksLOhYvhf9iArmiCUy89mNxo2CGO4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXZl/bG9wZXJzLmVsZW1l/bnRvci5jb20vZG9j/cy9hc3NldHMvaW1n/L2VsZW1lbnRvci1w/bGFjZWhvbGRlci1p/bWFnZS5wbmc" />`
         }
     <strong class="text-muted text-sm">Created on: ${date.toDateString()}</strong>
     <h2 class="mb-3">${title}</h2>
@@ -110,14 +77,14 @@ const updateLocalStorage = () => {
 
 //where we convert string back to JSON.......rendering cards on the screen
 const loadInitialData= () =>{
-    const localStorageCopy =JSON.parse(localStorage.tasks);
+    const localStorageCopy =JSON.parse(localStorage.task);
 
     if(localStorageCopy) state.taskList =localStorageCopy.tasks;
 
     state.taskList.map((cardDate)=>{
-        taskContents.innerAdjacentHTML("beforeEnd",htmlTaskContent(cardDate));
-    })
-}
+        taskContents.insertAdjacentHTML("beforeEnd",htmlTaskContent(cardDate));
+    });
+};
 
 //when we update or edit we have to save
 const handleSubmit=(event)=>{
@@ -125,11 +92,53 @@ const handleSubmit=(event)=>{
     const input={
         url: document.getElementById("imageUrl").value,
         title: document.getElementById("taskTitle").value,
-        tags:document.getElementById("tags").value,
-        taskDescription:document.getElementById("taskDescription").value,
+        type: document.getElementById("tags").value,
+        description: document.getElementById("taskDescription").value,
     };
-    taskContents.innerAdjacentHTML("beforeEnd",htmlTaskContent({...input, id}));
+    if (input.title==""||input.type==""||input.description==""){
+        return alert("Please fill all the neccessary fields")
+    }
+    
+    taskContents.insertAdjacentHTML("beforeEnd",htmlTaskContent({...input, id}));
     state.taskList.push({...input, id});
     
     updateLocalStorage();
 };
+
+
+const openTask=(e)=>{
+    if(!e) e = window.event;
+
+    const getTask =state.taskList.find(({id})=> id === e.target.id);
+    taskModal.innerHTML=htmlModalContent(getTask);
+}
+
+//Delete task
+const deleteTask=(e)=>{
+    if(!e) e = window.event;
+
+    const targetId= e.target.getAttribute("name");
+    //console.log(targetId)
+    const type= e.target.tagName;
+    const removeTask= state.taskList.filter(({id})=> id!==targetId);
+    //console.log(removeTask)
+    updateLocalStorage();
+
+    if(type==="BUTTON"){
+        return e.target.parentNode.parentNode.parentNode.parentNode.removeChild( 
+            e.target.parentNode.parentNode.parentNode
+        );
+    }else if(type==="I"){
+        return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild( 
+            e.target.parentNode.parentNode.parentNode.parentNode
+        );
+    }
+}
+
+
+
+
+
+
+
+
